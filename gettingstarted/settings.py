fd6584f246042fb53d06960437ef002092b9c933
Joseph Simonian <j.simonian10@gmail.com>
2017-06-24 15:15:28 -0700

    Change to liighter

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello'
+    'liighter_app'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -135,4 +135,3 @@ STATICFILES_DIRS = (
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -3,14 +3,14 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+import liighter_app.views
 
 # Examples:
 # url(r'^$', 'gettingstarted.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
+    url(r'^$', liighter_app.views.index, name='index'),
+    url(r'^db', liighter_app.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
 ]
